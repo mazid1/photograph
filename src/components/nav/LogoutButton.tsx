03c91ac8a5605f2a3c52c5caa5f7074dfea01378
Mazedul Islam <mazidmailbox@gmail.com>
2024-05-10 11:49:16 +0600

    redirect to root page after logout

--- src/components/nav/LogoutButton.tsx
+++ src/components/nav/LogoutButton.tsx
@@ -5,7 +5,9 @@ import { signOut } from "next-auth/react";
 function LogoutButton() {
   const reset = useLikeStore((store) => store.reset);
   const handleLogout = async () => {
-    await signOut();
+    await signOut({
+      callbackUrl: "/",
+    });
     reset();
   };
   return <button onClick={handleLogout}>Logout</button>;
