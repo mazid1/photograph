d8247ef6b53d0e1e6ceafe29a1e7dd16cdd5f1f3
Mazedul Islam <mazidmailbox@gmail.com>
2024-05-10 10:39:28 +0600

    liked photo visibility bug fixed

--- src/store/likeStoreProvider.tsx
+++ src/store/likeStoreProvider.tsx
@@ -1,7 +1,15 @@
 "use client";
-import { type ReactNode, createContext, useRef, useContext } from "react";
+import {
+  type ReactNode,
+  createContext,
+  useRef,
+  useContext,
+  useEffect,
+} from "react";
 import { type StoreApi, useStore } from "zustand";
 import { LikeStore, createLikeStore } from "./likeStore";
+import { getLikedPhotos } from "@/actions/getLikedPhotos";
+import { useSession } from "next-auth/react";
 
 export const LikeStoreContext = createContext<StoreApi<LikeStore> | null>(null);
 
@@ -11,6 +19,28 @@ export interface LikeStoreProviderProps {
 
 export const LikeStoreProvider = ({ children }: LikeStoreProviderProps) => {
   const storeRef = useRef<StoreApi<LikeStore>>();
+  const { status } = useSession();
+
+  useEffect(() => {
+    const fetchLikedPhotos = async () => {
+      const response = await getLikedPhotos();
+      if (response.success) {
+        if (storeRef.current) {
+          storeRef.current.setState({ liked: response.data! });
+        } else {
+          storeRef.current = createLikeStore({ liked: response.data! });
+        }
+      } else {
+        if (storeRef.current) {
+          storeRef.current.setState({ liked: {} });
+        } else {
+          storeRef.current = createLikeStore();
+        }
+      }
+    };
+    fetchLikedPhotos();
+  }, [status]);
+
   if (!storeRef.current) {
     storeRef.current = createLikeStore();
   }
