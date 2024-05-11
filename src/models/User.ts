ba479d2c6dbc150a3f51720f9433c61a485ffc59
Mazedul Islam <mazidmailbox@gmail.com>
2024-05-12 05:36:36 +0600

    confirm password in register form

--- src/models/User.ts
+++ src/models/User.ts
@@ -10,6 +10,7 @@ export const RegisterSchema = z.object({
   name: z.string().min(1, "Name is required"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
+  confirmPassword: z.string(),
 });
 
 export type RegisterFlattenedErrors = z.inferFlattenedErrors<
