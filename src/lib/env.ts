efbbcac88c20b83e5bb1b056b94bf44721eda3b7
Mazedul Islam <mazidmailbox@gmail.com>
2024-05-12 06:28:18 +0600

    unused env vars removed

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,4 @@ export const env = cleanEnv(process.env, {
   PEXELS_API_KEY: str(),
   NEXTAUTH_SECRET: str(),
   NODE_ENV: str({ default: "development" }),
-  NETLIFY_SITE_ID: str(),
-  NETLIFY_ACCESS_TOKEN: str(),
 });
