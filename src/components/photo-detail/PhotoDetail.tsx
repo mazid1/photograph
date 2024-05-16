4f7b39163dd59d91660be2d0769023433035f7e1
Mazedul Islam <mazidmailbox@gmail.com>
2024-05-16 10:45:34 +0600

    photo detail modal max width increased and only image is scrollable

--- src/components/photo-detail/PhotoDetail.tsx
+++ src/components/photo-detail/PhotoDetail.tsx
@@ -11,23 +11,27 @@ function PhotoDetail({ photo }: PhotoDetailProps) {
   const src = `/.netlify/images?url=${photo.src.original}&fit=cover&position=center`;
 
   return (
-    <div className="max-w-5xl min-w-64">
-      <h3 className="font-bold text-lg mb-2">{photo.alt}</h3>
-      <div className="flex flex-row mb-2 gap-2 justify-center">
-        <LikeButton photo={photo} />
-        <PhotoDownloader photo={photo} />
+    <div className="max-w-4xl flex flex-col">
+      <div className="flex flex-col items-center">
+        <h3 className="font-bold text-lg mb-2">{photo.alt}</h3>
+        <div className="flex flex-row flex-wrap mb-2 gap-2">
+          <LikeButton photo={photo} />
+          <PhotoDownloader photo={photo} />
+        </div>
       </div>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={src}
-        srcSet={`${src}&w=250 250w, ${src}&w=420 420w, ${src}&w=768 768w`}
-        sizes={"(max-width: 336px) 250px, (max-width: 640px) 420px, 768px"}
-        alt={photo.alt}
-        width={photo.width}
-        height={photo.height}
-        className="object-cover object-center"
-        style={{ backgroundColor: photo.avg_color }}
-      />
+      <div className="overflow-auto max-h-full">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={src}
+          srcSet={`${src}&w=250 250w, ${src}&w=420 420w, ${src}&w=768 768w`}
+          sizes={"(max-width: 336px) 250px, (max-width: 640px) 420px, 768px"}
+          alt={photo.alt}
+          width={photo.width}
+          height={photo.height}
+          className="object-cover object-center rounded"
+          style={{ backgroundColor: photo.avg_color }}
+        />
+      </div>
     </div>
   );
 }
--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -62,7 +62,7 @@ export const ModalContextProvider = ({ children }: PropsWithChildren) => {
     >
       {children}
       <dialog ref={modalRef} className="modal">
-        <div className="modal-box scale-100">
+        <div className="modal-box scale-100 max-w-4xl flex overflow-hidden">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <button
